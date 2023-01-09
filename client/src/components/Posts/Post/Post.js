8422eb5b667a45a9ab445e35c1b86e17965a882c
BillyMaysHere32 <kgetz32@gmail.com>
2023-01-09 10:31:09 -0800

    integrated posts and post logic so img and creater appear on home page,fix key prop error

--- client/src/components/Posts/Post/Post.js
+++ client/src/components/Posts/Post/Post.js
@@ -1,9 +1,28 @@
 import React from 'react'
+import { Button, Card } from 'react-bootstrap'
 
-export default function Post() {
+export default function Post({ post, setCurrentId }) {
+
   return (
-    <div>
-      <h1>Post</h1>
-    </div>
+    // <div>
+    //   <h1>Post</h1>
+    // </div>
+   
+
+<Card className='h-100'>
+<Card.Img variant="top" src={post.selectedFile} height="200px" style={{ objectFit: "cover"}} />
+
+<Card.Body className="d-flex flex-column">
+  <Card.Title className="d-flex justify-content-between align-items-baseline mb-4">
+      <span className="fs-2">{post.creator}</span>
+         
+  </Card.Title>
+  <div className='mt-auto'>
+
+    </div>  
+</Card.Body>
+</Card> 
+
+
   )
 }
--- client/src/components/Posts/Posts.js
+++ client/src/components/Posts/Posts.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import Post from './Post/Post'
-import { Container, } from "react-bootstrap";
+import { Container, Row, Col } from "react-bootstrap";
 import { useSelector } from 'react-redux';
 
 export default function Posts() {
@@ -9,18 +9,28 @@ export default function Posts() {
   console.log(posts);
 
   return (
-
     <Container>
         <h1>Recent Posts</h1>
-        <Post />
-        <Post />
+        {/* <Post />
+        <Post /> */}
+ 
+     <Row md={2} xs={1} lg={3} className="g-3">
+             {posts.map((post) => (
+              <Col key={post.id}>
+                <Post post={post} />
+            </Col>
+        ))}
+      </Row>
     </Container>
-    // <Row md={2} xs={1} lg={3} className="g-3">
-    //         {storeItems.map(item => (
-    //           <Col key={item.id}>
-    //             <Post {...item} />
-    //         </Col>
+
+    // !posts.length ? <CircularProgress /> : (
+    //   <Grid className={classes.container} container alignItems="stretch" spacing={3}>
+    //      {posts.map((post) => (
+    //       <Grid key={post._id} item xs={12} sm={6} md={6}>
+    //         <Post post={post} setCurrentId={setCurrentId} />
+    //       </Grid>
     //     ))}
-    //   </Row>
+    //   </Grid>
+    // )
   )
 }
