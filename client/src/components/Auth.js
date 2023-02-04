50680e9409f4a36244f06518eda45262d10c2bb7
BillyMaysHere32 <kgetz32@gmail.com>
2023-02-04 11:02:10 -0800

    added state to sign in form

--- client/src/components/Auth.js
+++ client/src/components/Auth.js
@@ -4,15 +4,21 @@ import Modal from 'react-bootstrap/Modal';
 import { useDispatch } from 'react-redux';
 import { AUTH } from '../constants/actionTypes';
 import jwt_decode from 'jwt-decode'
-import { useNavigate } from 'react-router-dom';
 
 import { Form } from "react-bootstrap"
 import Container from 'react-bootstrap/Container';
 import Button from 'react-bootstrap/Button';
 import FloatingLabel from 'react-bootstrap/FloatingLabel';
 
+
 export default function Auth() {
-      // modal state
+  // state 
+  const initialState = { firstName: '', lastName: '', userEmail: '', userPassword: '', confirmPassword: '' };
+
+  // sign up input state
+  const [userData, setUserData] = useState(initialState);
+  
+  // modal state
   const [show, setShow] = useState(false);
   const handleClose = () => setShow(false); 
   const handleShow = () => setShow(true);
@@ -22,8 +28,10 @@ export default function Auth() {
   const dispatch = useDispatch();
 
   const handleSubmit = (e) => {
-    e.preventDefault();
     // to avoid refresh
+    e.preventDefault();
+    
+    console.log(userData);
   };
 
   const switchMode = () => {
@@ -43,60 +51,73 @@ export default function Auth() {
                 <Modal.Title>{isSignUp ? "Sign Up" : "Sign In"}</Modal.Title>
             </Modal.Header>
             <Modal.Body>
-                <Container id="main-container" className="bg-light">
+
+                <Container id="sign-in-container" className="bg-light">
                     <Form onSubmit={handleSubmit} noValidate id="sign-up-form" className="text-center">
                         <h1 className="mb-3 fs-3 fw-normal">{isSignUp ? "Sign Up" : "Log In"}</h1>
+                        <div>
+                        {/* First and Last name inputs */}
                         { isSignUp && (
                             <>
                                 <FloatingLabel
-                                    controlId="first-name"
+                                    controlId="firstName"
                                     label="First Name"
                                     className="mb-3"
                                     ><Form.Control as="textarea" placeholder="Leave a message here" 
-                                    value="" 
-                                    // onChange={ => ()}
+                                    value={userData.firstName} 
+                                    onChange={(e) => setUserData({ ...userData, firstName: e.target.value })}
                                 />
                                 </FloatingLabel>
 
                                 <FloatingLabel
-                                    controlId="last-name"
+                                    controlId="lastName"
                                     label="Last Name"
                                     className="mb-3"
                                     ><Form.Control as="textarea" placeholder="Leave a message here" 
-                                    value="" 
-                                    // onChange={ => ()}
+                                    value={userData.lastName} 
+                                    onChange={(e) => setUserData({ ...userData, lastName: e.target.value })}
                                 />
                                 </FloatingLabel>
                             </>
                         )}
 
+                        {/* Email and Password inputs */}
                         <FloatingLabel
-                            controlId="floatingInput"
+                            controlId="userEmail"
                             label="Email"
                             className="mb-3"
-                        >
-                            <Form.Control type="email" placeholder="name@example.com" />
+                        ><Form.Control type="email" placeholder="name@example.com" 
+                            value={userData.userEmail}  
+                            onChange={(e) => setUserData({ ...userData, userEmail: e.target.value })}/>
                         </FloatingLabel>
 
-                        <FloatingLabel controlId="floatingPassword" label="Password">
-                            <Form.Control type="password" placeholder="Password" />
+                        <FloatingLabel controlId="userPassword" label="Password">
+                            <Form.Control type="password" placeholder="Password" 
+                                value={userData.userPassword}  
+                                onChange={(e) => setUserData({ ...userData, userPassword: e.target.value })}/>
                             <Form.Text id="passwordHelpBlock" muted>
                                 Your password must be 8-20 characters long, contain letters and numbers,
                                 and must not contain spaces, special characters, or emoji.
                             </Form.Text>
                         </FloatingLabel>
 
+                        {/* Confirm password in sign up input */}
                         { isSignUp &&
-                            <FloatingLabel controlId="floatingPassword" label="Confirm Password">
-                                <Form.Control type="password" placeholder="Password" />
+                            <FloatingLabel controlId="confirmPassword" label="Confirm Password">
+                                <Form.Control type="password" placeholder="Password" 
+                                    value={userData.confirmPassword}  
+                                    onChange={(e) => setUserData({ ...userData, confirmPassword: e.target.value })}/>
                             </FloatingLabel>
                         }
 
+                        {/* Form Buttons */}
                         <div className="d-grid mt-4 mb-2">
-                            <Button variant="primary" type="submit" size="lg">{isSignUp ? "Create Account" : "Log In"}</Button>
+                            <Button variant="primary" type="submit" size="lg">
+                                {isSignUp ? "Create Account" : "Log In"}</Button>
       
                             <Button variant="secondary" className="mt-2 mb-2" 
-                                onClick={switchMode}>{ isSignUp ? 'Already have an account? Log in' : "Don't have an account? Sign Up" }
+                                onClick={switchMode}>
+                                    { isSignUp ? 'Already have an account? Log in' : "Don't have an account? Sign Up" }
                             </Button>
 
                             <GoogleLogin
@@ -121,6 +142,7 @@ export default function Auth() {
                         </div>
 
                         <p className="mt-5 text-muted">&copy; 2022-2023</p>
+                        </div>
                     </Form>
                 </Container>
             </Modal.Body>
